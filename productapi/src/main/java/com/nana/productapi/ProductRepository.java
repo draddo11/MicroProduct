05940205ad1419d5982a5a476e2200d5adc37f5f
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-07-19 22:37:10 +0000

    updated the files

---
+++ productapi/src/main/java/com/nana/productapi/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.nana.productapi;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+
+@RepositoryRestResource(collectionResourceRel = "productdata",
+        path = "productdata")
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
